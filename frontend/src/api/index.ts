aeb07166028b229b4f2e8b9c4bed670f0a6fa408
Assaf Haik Barouch <assaf98@gmail.com>
2026-03-12 16:38:39 +0200

    moved the base API to an environment var

--- frontend/src/api/index.ts
+++ frontend/src/api/index.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import type { Token, Project, Ticket, User } from '../models';
 
-const API_BASE_URL = 'http://localhost:8000';
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:8000';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
